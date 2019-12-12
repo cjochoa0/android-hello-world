8779b76f07fe735d3e1d3c25ce3b2e69a6d276ac
Coope <cjochoa@ualr.edu>
2019-12-12 15:01:34 -0600

    pushing work

---
+++ app/src/main/java/com/ualr/helloworld/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ualr.helloworld;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView userMsgTV;
+    private EditText userInputET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userMsgTV = findViewById(R.id.userMsgTV);
+        userInputET = findViewById(R.id.userInputET);
+    }
+
+    public void showTextMessage(View view) {
+        if(!userInputET.getText().toString().isEmpty())
+            userMsgTV.setText(userInputET.getText().toString());
+        else{
+
+        }
+
+    }
+
+    public void clearTextMessage(View view){
+        userMsgTV.setText("");
+    }
+
+}
